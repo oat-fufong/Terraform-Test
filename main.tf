0ca9b300c8483c98d5d720094285e5c195cd9bbb
Oat <natakorn.s@fufonglabs.com>
2026-08-11 16:02:55 +0700

    use tfstate on gcloud

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "6.8.0"
     }
   }
+
+  # State now lives in GCS instead of the local terraform.tfstate file.
+  # NOTE: backend blocks can't use variables
+  backend "gcs" {
+    bucket = "c0001-uat-terraform-state"
+    prefix = "terraform/state"
+    impersonate_service_account = "c0001-uat-terraform-deployer@c0001-uat.iam.gserviceaccount.com"
+  }
 }
 
 provider "google" {
