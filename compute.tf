c28e2c93dff2c47dda317255e71e4557791de9d5
Oat <natakorn.s@fufonglabs.com>
2026-08-13 10:28:13 +0700

    tf file for VM instances

---
+++ compute.tf
@@ -0,0 +1,35 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "${var.machine_type}"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  metadata = {
+    enable-oslogin = "TRUE"
+    startup-script = file("install_docker.sh")
+  }
+
+  tags = ["lb-backend"]
+}
+
+resource "google_compute_instance_group" "app_ig" {
+  name = "app-instance-group"
+  zone = var.zone
+
+  instances = [google_compute_instance.vm_instance.self_link]
+
+  named_port {
+    name = "http"
+    port = var.backend_port
+  }
+}
