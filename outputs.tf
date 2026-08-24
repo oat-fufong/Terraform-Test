29bd36381ce22087aea7683e242603ad4b199b22
Oat <natakorn.s@fufonglabs.com>
2026-08-24 13:56:48 +0700

    remove duplicate boostrap bucket/API tracking from root config

--- outputs.tf
+++ outputs.tf
@@ -37,8 +37,3 @@ output "ssh_command" {
   description = "gcloud command to SSH into the VM via the IAP tunnel"
   value       = "gcloud compute ssh ${google_compute_instance.vm_instance.name} --zone=${var.zone} --project=${var.gcp_project} --tunnel-through-iap"
 }
-
-output "api_statuses" {
-  description = "Which APIs were enabled"
-  value       = { for k, v in google_project_service.apis : k => "ENABLED" }
-}
